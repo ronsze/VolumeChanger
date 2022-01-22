f3c671e27ade023d26f02bac2cb1600db11a4208
zeorns <ljg5846@gmail.com>
2022-01-23 01:59:25 +0900

    UI수정

--- app/src/main/java/com/example/volumechanger/MainActivity.kt
+++ app/src/main/java/com/example/volumechanger/MainActivity.kt
@@ -13,6 +13,8 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.Button
 import androidx.core.app.ActivityCompat
 import com.example.volumechanger.databinding.ActivityMainBinding
 import com.naver.maps.geometry.LatLng
